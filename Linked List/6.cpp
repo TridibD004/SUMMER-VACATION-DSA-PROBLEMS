4dd141314a0335b4a2bc312a60c495275bc5494a
Tridib Dalui <tridibdalui04@gmail.com>
2022-06-26 19:10:07 +0530

    Create 6.cpp

---
+++ Linked List/6.cpp
@@ -0,0 +1,130 @@
+/*
+PROBLEM :- Finding middle element in a linked list
+
+Given a singly linked list of N nodes.
+The task is to find the middle of the linked list. For example, if the linked list is
+1-> 2->3->4->5, then the middle node of the list is 3.
+If there are two middle nodes(in case, when N is even), print the second middle element.
+For example, if the linked list given is 1->2->3->4->5->6, then the middle node of the list is 4.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+Output: 3 
+Explanation: 
+Middle of linked list is 3.
+Example 2: 
+
+Input:
+LinkedList: 2->4->6->7->5->1
+Output: 7 
+Explanation: 
+Middle of linked list is 7.
+Your Task:
+The task is to complete the function getMiddle() which takes a head reference as the only argument and should return the data at the middle node of the linked list.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).   */
+
+//CODE :- 
+
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+    }  
+    cout<<"\n";
+} 
+
+
+ // } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+class Solution{
+    public:
+    /* Should return data of middle node. If linked list is empty, then  -1*/
+    int getMiddle(Node *head)
+    {
+        // Your code here
+        Node *p=head;
+        Node *q=head;
+        if(head==NULL)
+        return -1;
+        else
+        {
+            int c=0;
+            while(p!=NULL)
+            {
+                p=p->next;
+                c++;
+            }
+        int x=0;
+        int y=c;
+        if(y%2==0)
+        y=(y+1)/2;
+        else
+        y=y/2;
+        while(x<y)
+        {
+            q=q->next;
+            x++;
+        }
+        return q->data;
+        }
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+    //code
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int data;
+        cin>>data;
+        struct Node *head = new Node(data);
+        struct Node *tail = head;
+        for (int i = 0; i < N-1; ++i)
+        {
+            cin>>data;
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        cout << ob.getMiddle(head) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
